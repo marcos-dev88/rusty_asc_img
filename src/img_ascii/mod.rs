b37aa5a00388ab19d8ce84de3917e8b3cfa5a301
marcos-dev88 <mv.piesske@gmail.com>
2022-12-28 23:35:14 -0300

    first development

---
+++ src/img_ascii/mod.rs
@@ -0,0 +1,47 @@
+const ASCII_LIST_LENGTH: usize = 15;
+
+const ASCII_LIST: [&str; ASCII_LIST_LENGTH] = [
+    " ", ".", ",", "-", "~", "+", "=", "7", "8", "9", "$", "W", "#", "@", "Ñ",
+];
+
+const RGB_8_BYTE_VAL_FLOAT: f32 = 255.0;
+
+pub fn gen_ascii_art(path: &str, width: u32, heigth: u32) -> String {
+    let img = image::open(path).unwrap();
+
+    let mut img_buff: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = image::imageops::resize(&img, width, heigth, image::imageops::FilterType::Lanczos3);
+
+    if img.width() >= width || img.height() >= heigth {
+        img_buff = image::imageops::resize(&img, width, heigth, image::imageops::FilterType::Nearest);
+    }
+
+    let mut count_x: u32 = 0;
+    let mut img_content = String::new();
+    let density: f32 = (ASCII_LIST_LENGTH - 1) as f32;
+
+    for p in img_buff.pixels() {
+        if count_x == img_buff.width() {
+            img_content.push('\n');
+            count_x = 0; 
+        }
+
+        let mut rgb_color: f32 = (density * get_rgb_byte_color(p[0], p[1], p[2])).round();
+
+        if p[3] == 0 {
+            rgb_color = 0.0;
+        }
+
+        img_content.push_str(ASCII_LIST[rgb_color as usize]);
+        count_x += 1;
+    }
+
+    img_content
+}
+
+fn get_rgb_byte_color(pixel_r: u8, pixel_g: u8, pixel_b: u8) -> f32 {
+    let pixel_r_f: f32 = (pixel_r / 3) as f32;
+    let pixel_g_f: f32 = (pixel_g / 3) as f32;
+    let pixel_b_f: f32 = (pixel_b / 3) as f32;
+    let rgb_color: f32 = pixel_r_f + pixel_g_f + pixel_b_f;
+    rgb_color / RGB_8_BYTE_VAL_FLOAT
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::File;
+use std::io::Write;
+pub mod img_ascii;
+
 fn main() {
-    println!("Hello, world!");
+    let img_str = img_ascii::gen_ascii_art("../../img/cargo_crab.png", 75, 30);
+    let file_name = "../../img/test.txt";
+    println!("{}", img_str);
+    let mut file = File::create(file_name).unwrap();
+    writeln!(file, "{}", img_str).unwrap();
 }

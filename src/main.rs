a1001f8a0cd4556f90045794141f0742ef42c274
marcos-dev88 <mv.piesske@gmail.com>
2023-01-21 16:31:40 -0300

    making main accept new input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::fs::File;
-use std::io::Write;
 use std::env;
 
 pub mod img_ascii;
@@ -7,10 +5,10 @@ pub mod cli;
 
 fn main() {
     let cli_p = cli::collect_cli_param::get_params(env::args().collect());
-    let new_img_ascii = img_ascii::new_image_ascii(*cli_p.width(), *cli_p.heigth());
+    let new_img_ascii = img_ascii::new_image_ascii(*cli_p.width(), *cli_p.heigth(), *cli_p.reversed_ascii());
     let img_str = new_img_ascii.gen_art(cli_p.path());
-    let file_name = "./img/test.txt";
+    // let file_name = "./img/test.txt";
     println!("{}", img_str);
-    let mut file = File::create(file_name).unwrap();
-    writeln!(file, "{}", img_str).unwrap();
+    // let mut file = File::create(file_name).unwrap();
+    // writeln!(file, "{}", img_str).unwrap();
 }

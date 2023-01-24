360f93ce621f6809ecbe34d513eccd86e5bf8dd4
marcos-dev88 <mv.piesske@gmail.com>
2023-01-23 23:49:58 -0300

    updating error handling in cli module

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
- pub mod collect_cli_param;
+pub mod collect_cli_param;
 
- #[derive(Debug)]
- pub struct CliParams { 
+#[derive(Debug)]
+pub struct CliParams {
     path: String,
     width: u32,
     heigth: u32,
@@ -14,44 +14,41 @@ impl CliParams {
         &self.path
     }
 
-    pub fn width(&self) -> &u32{ 
+    pub fn width(&self) -> &u32 {
         &self.width
     }
 
-     pub fn heigth(&self) -> &u32{
+    pub fn heigth(&self) -> &u32 {
         &self.heigth
     }
-    
-     pub fn reversed_ascii(&self) -> &bool {
-         &self.reversed_ascii
-     }
+
+    pub fn reversed_ascii(&self) -> &bool {
+        &self.reversed_ascii
+    }
 
     pub fn colorized(&self) -> &bool {
-         &self.colorized
-     }
-
+        &self.colorized
+    }
 }
 
-impl CliParams{ 
-     fn set_path(&mut self) -> &mut String {
+impl CliParams {
+    fn set_path(&mut self) -> &mut String {
         &mut self.path
     }
 
-     fn set_width(&mut self) -> &mut u32{ 
+    fn set_width(&mut self) -> &mut u32 {
         &mut self.width
     }
 
-     fn set_heigth(&mut self) -> &mut u32{
+    fn set_heigth(&mut self) -> &mut u32 {
         &mut self.heigth
     }
 
     fn set_reversed_ascii(&mut self) -> &mut bool {
-         &mut self.reversed_ascii
-     }
+        &mut self.reversed_ascii
+    }
 
     fn set_colorized(&mut self) -> &mut bool {
         &mut self.colorized
     }
-
 }
-
--- src/cli/collect_cli_param.rs
+++ src/cli/collect_cli_param.rs
@@ -1,68 +1,89 @@
 use crate::cli::CliParams;
-use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::{collections::HashMap, fmt::Error};
 
 const DEFAULT_WIDTH: u32 = 75;
 const DEFAULT_HEIGTH: u32 = 35;
 const EMPTY_PARAM_VALUE: i32 = -1;
 
-pub fn get_params(params: Vec<String>) -> CliParams { 
-    let mut cli_p: CliParams = CliParams{path: String::new(), width: DEFAULT_WIDTH, heigth: DEFAULT_HEIGTH, reversed_ascii: false, colorized: false};
-    let map_index = get_param_index(&params);
+const PATH_FLAG: &str = "-p";
+const WIDTH_FLAG: &str = "-w";
+const HEIGHT_FLAG: &str = "-h";
+const COLORIZED_FLAG: &str = "-c";
+const REVERSED_FLAG: &str = "-rev";
 
-    if map_index["-p"] != EMPTY_PARAM_VALUE { 
-        *cli_p.set_path() = params[(map_index["-p"]+1) as usize].parse().unwrap();
+pub fn get_params(params: Vec<String>) -> Result<CliParams, Error> {
+    let mut cli_p: CliParams = CliParams {
+        path: String::new(),
+        width: DEFAULT_WIDTH,
+        heigth: DEFAULT_HEIGTH,
+        reversed_ascii: false,
+        colorized: false,
+    };
+
+    let map_index = match get_param_index(&params) {
+        Ok(map_index) => map_index,
+        Err(e) => {
+            panic!("error to get params index: {}", e)
+        }
+    };
+
+    if map_index.get(PATH_FLAG).is_some() && map_index[PATH_FLAG] != EMPTY_PARAM_VALUE {
+        *cli_p.set_path() = match params[(map_index[PATH_FLAG] + 1) as usize].parse() {
+            Ok(val) => val,
+            Err(e) => {
+                panic!("error on get path value: {}", e)
+            }
+        };
     }
 
-    if map_index["-w"] != EMPTY_PARAM_VALUE { 
-        *cli_p.set_width() = params[(map_index["-w"]+1) as usize].parse().unwrap();
+    if map_index.get(WIDTH_FLAG).is_some() && map_index[WIDTH_FLAG] != EMPTY_PARAM_VALUE {
+        *cli_p.set_width() = match params[(map_index[WIDTH_FLAG] + 1) as usize].parse() {
+            Ok(val) => val,
+            Err(e) => {
+                panic!("error on get path value: {}", e)
+            }
+        };
     }
 
-    if map_index["-h"] != EMPTY_PARAM_VALUE { 
-        *cli_p.set_heigth() = params[(map_index["-h"]+1) as usize].parse().unwrap();
+    if map_index.get(HEIGHT_FLAG).is_some() && map_index[HEIGHT_FLAG] != EMPTY_PARAM_VALUE {
+        *cli_p.set_heigth() = match params[(map_index[HEIGHT_FLAG] + 1) as usize].parse() {
+            Ok(val) => val,
+            Err(e) => {
+                panic!("error on get path value: {}", e)
+            }
+        };
     }
 
-    if map_index["-rev"] != EMPTY_PARAM_VALUE { 
+    if map_index.get(REVERSED_FLAG).is_some() && map_index[REVERSED_FLAG] != EMPTY_PARAM_VALUE {
         *cli_p.set_reversed_ascii() = true;
     }
 
-    if map_index["-c"] != EMPTY_PARAM_VALUE { 
+    if map_index.get(COLORIZED_FLAG).is_some() && map_index[COLORIZED_FLAG] != EMPTY_PARAM_VALUE {
         *cli_p.set_colorized() = true;
-    } 
+    }
 
-    cli_p
+    Ok(cli_p)
 }
 
-fn get_param_index(params: &[String]) -> HashMap<&str, i32>{
+fn get_param_index(params: &[String]) -> Result<HashMap<&str, i32>, Error> {
     let mut map_index: HashMap<&str, i32> = Default::default();
 
-    map_index.insert("-p", EMPTY_PARAM_VALUE);
-    map_index.insert("-w", EMPTY_PARAM_VALUE);
-    map_index.insert("-h", EMPTY_PARAM_VALUE);
-    map_index.insert("-rev", EMPTY_PARAM_VALUE);
-    map_index.insert("-c", EMPTY_PARAM_VALUE);
- 
-    for (i, param) in params.iter().enumerate(){
+    for (i, param) in params.iter().enumerate() {
+        if param == PATH_FLAG
+            || param == WIDTH_FLAG
+            || param == HEIGHT_FLAG
+            || param == REVERSED_FLAG
+            || param == COLORIZED_FLAG
+        {
+            let param_value = match map_index.entry(param) {
+                Vacant(entry) => entry.insert(EMPTY_PARAM_VALUE),
+                Occupied(entry) => entry.into_mut(),
+            };
 
-        if param == "-p"{
-            *map_index.get_mut("-p").unwrap() = i as i32;
-        }
-
-        if param == "-w"{
-            *map_index.get_mut("-w").unwrap() = i as i32;
-        }
-
-        if param == "-h"{
-            *map_index.get_mut("-h").unwrap() = i as i32;
-        }
-
-        if param == "-rev"{
-            *map_index.get_mut("-rev").unwrap() = i as i32;
-        }
-
-        if param == "-c"{
-            *map_index.get_mut("-c").unwrap() = i as i32;
+            *param_value = i as i32;
         }
     }
 
-    map_index
+    Ok(map_index)
 }

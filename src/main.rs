437307183fdf6cb10c3c72360a95fd79d4e16f85
marcos-dev88 <mv.piesske@gmail.com>
2023-01-23 23:51:19 -0300

    updating error handling main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 use std::env;
 
-pub mod img_ascii;
 pub mod cli;
+pub mod img_ascii;
 
 fn main() {
-    let cli_p = cli::collect_cli_param::get_params(env::args().collect());
-    let new_img_ascii = img_ascii::new_image_ascii(*cli_p.width(), *cli_p.heigth(), *cli_p.reversed_ascii(), *cli_p.colorized());
-    let img_str = new_img_ascii.gen_art(cli_p.path());
+    let cli_p = match cli::collect_cli_param::get_params(env::args().collect()) {
+        Ok(cli_p) => cli_p,
+        Err(e) => {
+            panic!("error to get params: {}", e)
+        }
+    };
+
+    let new_img_ascii = img_ascii::new_image_ascii(
+        *cli_p.width(),
+        *cli_p.heigth(),
+        *cli_p.reversed_ascii(),
+        *cli_p.colorized(),
+    );
+
+    match new_img_ascii.gen_art(cli_p.path()) {
+        Ok(img_str) => {
+            println!("{}", img_str);
+        }
+        Err(e) => {
+            panic!("error to gen ascii art: {}", e)
+        }
+    };
     // let file_name = "./img/test.txt";
-    println!("{}", img_str);
     // let mut file = File::create(file_name).unwrap();
     // writeln!(file, "{}", img_str).unwrap();
 }
